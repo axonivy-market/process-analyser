a29ec5b6e7aa8c185bdb55d3a6fb23d69c32af16
Dinh Nguyen <127725498+ntqdinh-axonivy@users.noreply.github.com>
2024-08-12 16:40:58 +0700

    Feature/marp 722 processmining data preparation for alternatives subs

--- bpmn-statistic/src/com/axonivy/utils/bpmnstatistic/constants/ProcessMonitorConstants.java
+++ bpmn-statistic/src/com/axonivy/utils/bpmnstatistic/constants/ProcessMonitorConstants.java
@@ -1,8 +1,6 @@
 package com.axonivy.utils.bpmnstatistic.constants;
 
 public class ProcessMonitorConstants {
-  public static final String PORTAL_START_REQUEST_PATH = "/DefaultApplicationHomePage.ivp";
-  public static final String PORTAL_IN_TEAMS_REQUEST_PATH = "InTeams.ivp";
   public static final String REMOVE_DEFAULT_HIGHLIGHT_JS_FUNCTION = "santizeDiagram();";
   public static final String UPDATE_FREQUENCY_COUNT_FOR_TASK_FUNCTION = "addElementFrequency('%s', '%s', '%s', '%s');";
   public static final String FREQUENCY_BACKGROUND_COLOR_LEVEL_VARIABLE_PATTERN = "frequencyBackgroundColorLevel%s";
@@ -12,4 +10,7 @@ public class ProcessMonitorConstants {
   public static final String UPDATE_ADDITIONAL_INFORMATION_FUNCTION = "renderAdditionalInformation('%s')";
   public static final int DEFAULT_INITIAL_STATISTIC_NUMBER = 0;
   public static final String HYPHEN_SIGN = "-";
+  public static final String QUOTED_CONTENT_PATTERN = "\"([^\"]*)\"";
+  public static final String BPMN_STATISTIC_PROJECT_NAME = "bpmn-statistic";
+  public static final String PORTAL_PROJECT_SUFFIX = "portal";
 }
