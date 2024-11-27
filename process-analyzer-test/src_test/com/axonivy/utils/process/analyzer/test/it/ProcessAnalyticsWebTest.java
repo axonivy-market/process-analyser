178a3add63efbe637b684b3067b510ece3f5c892
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-11-27 16:41:53 +0700

    update waiting time to avoid fail test

--- process-analyzer-test/src_test/com/axonivy/utils/process/analyzer/test/it/ProcessAnalyticsWebTest.java
+++ process-analyzer-test/src_test/com/axonivy/utils/process/analyzer/test/it/ProcessAnalyticsWebTest.java
@@ -17,10 +17,10 @@ public class ProcessAnalyticsWebTest {
   private final String TEST_PROCESS_PATH = "/process-analyzer-test/193485C5ABDFEA93/test.ivp";
   private final String SHOW_STATISTIC_BTN_CSS_SELECTOR = "#process-analytics-form\\:show-statistic-btn";
   private final String SHOW_MODULE_DROPDOWN_BTN_CSS_SELECTOR = "#process-analytics-form\\:moduleDropdown .ui-selectonemenu-trigger";
-  private final String SHOW_PROCESS_DROPDOWN_BTN_CSS_SELECTOR ="#process-analytics-form\\:processDropdown .ui-selectonemenu-trigger";
-  private final String SHOW_KPI_DROPDOWN_BTN_CSS_SELECTOR ="#process-analytics-form\\:kpiDropdown .ui-selectonemenu-trigger";
+  private final String SHOW_PROCESS_DROPDOWN_BTN_CSS_SELECTOR = "#process-analytics-form\\:processDropdown .ui-selectonemenu-trigger";
+  private final String SHOW_KPI_DROPDOWN_BTN_CSS_SELECTOR = "#process-analytics-form\\:kpiDropdown .ui-selectonemenu-trigger";
   private final String UI_DROPDOWN_MENU_LIST_CSS_SELECTOR = ".ui-selectonemenu-items li";
-  
+
   @Test
   void showStatisticButtonShouldEnableWhenChosenFulfiled() {
     // Open process analyzer view
@@ -31,17 +31,19 @@ public class ProcessAnalyticsWebTest {
 
     // Chose module, process, kpi
     $(SHOW_MODULE_DROPDOWN_BTN_CSS_SELECTOR).click();
+    Selenide.sleep(200);
     $$(UI_DROPDOWN_MENU_LIST_CSS_SELECTOR).get(1).click();
     Selenide.sleep(200);
     $(SHOW_PROCESS_DROPDOWN_BTN_CSS_SELECTOR).click();
+    Selenide.sleep(200);
     $$(UI_DROPDOWN_MENU_LIST_CSS_SELECTOR).get(1).click();
     Selenide.sleep(200);
     $(SHOW_KPI_DROPDOWN_BTN_CSS_SELECTOR).click();
+    Selenide.sleep(200);
     $$(UI_DROPDOWN_MENU_LIST_CSS_SELECTOR).get(1).click();
     Selenide.sleep(200);
-
-    //Check the status of show statistic btn after data fulfilled
+    // Check the status of show statistic btn after data fulfilled
     $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute("disabled", ""));
-  }
 
+  }
 }
