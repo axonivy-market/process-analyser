2d584561c0a1ed1a8a50e1f21a7d1877d62fc5cc
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-11-28 14:04:18 +0700

    handle feedback

--- process-analyser-test/src_test/com/axonivy/utils/process/analyser/test/it/ProcessAnalyticsWebTest.java
+++ process-analyser-test/src_test/com/axonivy/utils/process/analyser/test/it/ProcessAnalyticsWebTest.java
@@ -6,7 +6,6 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
-import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.environment.IvyTest;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -15,9 +14,11 @@ import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Condition.visible;
 
 import static com.codeborne.selenide.Condition.attribute;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
 @IvyWebTest
@@ -25,13 +26,13 @@ import org.junit.jupiter.api.Test;
 public class ProcessAnalyticsWebTest {
   private final String TEST_PROCESS_PATH = "/process-analyser-test/193485C5ABDFEA93/test.ivp";
   private final String SHOW_STATISTIC_BTN_CSS_SELECTOR = "#process-analytics-form\\:show-statistic-btn";
-
   private final String MODULE_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:moduleDropdown";
   private final String PROCESS_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:processDropdown";
   private final String KPI_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:kpiDropdown";
-  private final String DROPDOWN_ITEMS_CSS_SELECTOR_SUFFIX = "_items li";
-//  private final String DROPDOWN_TRIGGER_DIV_CSS_SELECTOR_SUFFIX = " > .ui-selectonemenu-trigger";
+  private final String DROPDOWN_LIST_SUFFIX = "_items";
+  private final String DROPDOWN_ITEMS_CSS_SELECTOR_SUFFIX = DROPDOWN_LIST_SUFFIX + " li";
   private final String DROPDOWN_LABEL_CSS_SELECTOR_SUFFIX = "_label";
+  private final String DISABLE_PROPERTY = "disabled";
 
   @Test
   void showStatisticButtonShouldEnableWhenChosenFulfiled() {
@@ -39,29 +40,31 @@ public class ProcessAnalyticsWebTest {
     open(EngineUrl.createProcessUrl(TEST_PROCESS_PATH));
 
     // Check the current status of show statistic button
-    $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute("disabled", "true"));
+    $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute(DISABLE_PROPERTY, "true"));
 
     clickFirstOptionFromTheDropdown(MODULE_DROPDOWN_CSS_SELECTOR);
     clickFirstOptionFromTheDropdown(PROCESS_DROPDOWN_CSS_SELECTOR);
     clickFirstOptionFromTheDropdown(KPI_DROPDOWN_CSS_SELECTOR);
 
-    // Check the status of show statistic btn after data fulfilled
-    $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute("disabled", ""));
+    // Check the status of show statistic button after data fulfilled
+    $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute(DISABLE_PROPERTY, StringUtils.EMPTY));
   }
 
   private void clickFirstOptionFromTheDropdown(String dropdownCssSelector) {
-    // Click the open dropdown icon
+    // Click target drop down when it's ready
     var dropdown = $(dropdownCssSelector);
-    dropdown.shouldBe(Condition.visible);
+    dropdown.shouldBe(visible);
     Selenide.sleep(1000);
     dropdown.click();
-    $(dropdownCssSelector+"_items").shouldBe(Condition.visible, Duration.ofSeconds(2));
+    $(dropdownCssSelector + DROPDOWN_LIST_SUFFIX).shouldBe(visible, Duration.ofSeconds(2));
+
     // Find 1st option (index = 1 to avoid choosing default initial option of null)
     SelenideElement targetElement = $$(dropdownCssSelector + DROPDOWN_ITEMS_CSS_SELECTOR_SUFFIX).get(1);
     String selectedOptionLabel = targetElement.text();
     targetElement.click();
 
     // Check if the label have been change to target option label
-    $(dropdownCssSelector + DROPDOWN_LABEL_CSS_SELECTOR_SUFFIX).shouldHave(Condition.text(selectedOptionLabel), Duration.ofSeconds(1));
+    $(dropdownCssSelector + DROPDOWN_LABEL_CSS_SELECTOR_SUFFIX).shouldHave(Condition.text(selectedOptionLabel),
+        Duration.ofSeconds(1));
   }
 }
