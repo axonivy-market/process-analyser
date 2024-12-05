213840168769a5ba500f68157fc1aa61faa2ad5e
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-12-05 18:29:18 +0700

    handle feedback

--- process-analyser/src/com/axonivy/solutions/process/analyser/constants/ProcessAnalyticViewComponentId.java
+++ process-analyser/src/com/axonivy/solutions/process/analyser/constants/ProcessAnalyticViewComponentId.java
@@ -11,7 +11,7 @@ public class ProcessAnalyticViewComponentId {
   public static final String PROCESS_ANALYTIC_VIEWER_PANEL = FORM + ID_SECTION_SEPARATOR
       + "process-analytic-viewer-panel";
   public static final String SHOW_STATISTIC_BTN = FORM + ID_SECTION_SEPARATOR + "show-statistic-btn";
-  public static final String CUSTOM_FILTER_PANEL = FORM + ID_SECTION_SEPARATOR + "custom-filter-panel";
+  public static final String CUSTOM_FILTER_PANEL = FORM + ID_SECTION_SEPARATOR + "custom-filter-panel-group";
   public static final String CUSTOM_FILTER_GROUP = CUSTOM_FILTER_PANEL + ID_SECTION_SEPARATOR + "custom-filter-group";
   public static final String CUSTOM_FILTER_OPTIONS_GROUP = CUSTOM_FILTER_PANEL + ID_SECTION_SEPARATOR
       + "filter-options-group";
