7dbc89d01b166a2120cd9cee4349d782ade932ec
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-06-03 07:54:59 +0700

    MARP-2313 Handle Feedbacks

--- process-analyser/src/com/axonivy/solutions/process/analyser/utils/DateUtils.java
+++ process-analyser/src/com/axonivy/solutions/process/analyser/utils/DateUtils.java
@@ -18,11 +18,11 @@ public class DateUtils {
   public static final String DATE_PATTERN = "dd/MM/yyyy";
   public static final String DATE_TIME_PATTERN = DATE_PATTERN + StringUtils.SPACE + TIME_PATTERN;
   private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_TIME_PATTERN);
-  private static final int SIXTY_MINUTES = 3600;
-  private static final int SIXTY_SECONDS = 60;
+  private static final int SECONDS_PER_HOUR = 3600;
+  private static final int SECONDS_PER_MINUTE = 60;
   private static final int FIFTY_NINE_MINUTES = 59;
   private static final int TWENTY_THREE_HOURS = 23;
-  private static final int TWENTY_FOUR_HOURS = 24;
+  private static final int HOURS_PER_DAY = 24;
 
   private DateUtils() {}
 
@@ -50,14 +50,14 @@ public class DateUtils {
 
   public static String convertDuration(float durationSeconds) {
     String result = StringUtils.EMPTY;
-    if (durationSeconds > TWENTY_THREE_HOURS * SIXTY_MINUTES) {
-      float days = durationSeconds / (TWENTY_FOUR_HOURS * SIXTY_MINUTES);
+    if (durationSeconds > TWENTY_THREE_HOURS * SECONDS_PER_HOUR) {
+      float days = durationSeconds / (HOURS_PER_DAY * SECONDS_PER_HOUR);
       result = formatFloat(days) + "d";
-    } else if (durationSeconds > FIFTY_NINE_MINUTES * SIXTY_SECONDS) {
-      float hours = durationSeconds / SIXTY_MINUTES;
+    } else if (durationSeconds > FIFTY_NINE_MINUTES * SECONDS_PER_MINUTE) {
+      float hours = durationSeconds / SECONDS_PER_HOUR;
       result = formatFloat(hours) + "h";
     } else if (durationSeconds > FIFTY_NINE_MINUTES) {
-      float minutes = durationSeconds / SIXTY_SECONDS;
+      float minutes = durationSeconds / SECONDS_PER_MINUTE;
       result = formatFloat(minutes) + "m";
     } else {
       result = formatFloat(durationSeconds) + "s";
