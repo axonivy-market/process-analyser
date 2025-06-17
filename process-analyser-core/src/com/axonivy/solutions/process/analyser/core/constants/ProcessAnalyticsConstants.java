fb8b109bb263b8fb830ec2646d6b4cf720c8bcf2
Thuy Nguyen Ngoc Thanh <thuy.nguyenngocthanh@axonactive.com>
2025-06-17 13:47:43 +0700

    Extract const

--- process-analyser-core/src/com/axonivy/solutions/process/analyser/core/constants/ProcessAnalyticsConstants.java
+++ process-analyser-core/src/com/axonivy/solutions/process/analyser/core/constants/ProcessAnalyticsConstants.java
@@ -11,6 +11,7 @@ public class ProcessAnalyticsConstants {
   public static final String PORTAL_PMV_SUFFIX = "portal";
   public static final String FROM = "from";
   public static final String TO = "to";
+  public static final String COMMA = ",";
   public static final String COMMA_CONNECT_PATTERN = ", ";
   public static final String CURRENT_TIME_PATTERN = "%s - %s %s";
   public static final String WHOLE_DAY_PATTERN = "%s - %s, %s %s, %s";
