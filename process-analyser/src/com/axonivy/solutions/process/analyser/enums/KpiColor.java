bd172028987ffbb9c4c93b86c3653d1190c575ab
Thuy Nguyen Ngoc Thanh <thuy.nguyenngocthanh@axonactive.com>
2025-06-19 09:33:57 +0700

    Change to use baseColor

--- process-analyser/src/com/axonivy/solutions/process/analyser/enums/KpiColor.java
+++ process-analyser/src/com/axonivy/solutions/process/analyser/enums/KpiColor.java
@@ -2,17 +2,22 @@ package com.axonivy.solutions.process.analyser.enums;
 
 import java.util.List;
 
+import com.axonivy.solutions.process.analyser.core.constants.ProcessAnalyticsConstants;
+import com.axonivy.solutions.process.analyser.utils.ColorUtils;
+
 public enum KpiColor implements HasCmsName {
-  FREQUENCY(List.of("#EDFAF1", "#C8F0D4", "#ADE8BF", "#87DEA1", "#70D78F", "#4CCD73", "#45BB69", "#369252", "#2A713F",
-      "#205630")),
+  FREQUENCY("rgb(70, 180, 100)"), DURATION("rgb(255, 206, 123)");
 
-  DURATION(List.of("#FFF7EA", "#FFEDCD", "#FFDEA5", "#FFCE7B", "#FFC054", "#FFB22E", "#D99727", "#B57E21", "#91651A",
-      "#735015"));
-
+  private final String baseColor;
   private final List<String> colors;
 
-  KpiColor(List<String> colors) {
-    this.colors = colors;
+  private KpiColor(String baseColor) {
+    this.baseColor = baseColor;
+    this.colors = ColorUtils.generateGradientFromRgb(baseColor, ProcessAnalyticsConstants.GRADIENT_COLOR_LEVELS);
+  }
+
+  public String getBaseColor() {
+    return baseColor;
   }
 
   public List<String> getColors() {
