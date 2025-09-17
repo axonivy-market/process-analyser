de500435a4acfcced2934f98e579d7f5f9f1b33f
Hoan Nguyen <hoan.nguyen01@axonactive.com>
2025-09-17 17:20:00 +0700

    Fix tests

--- process-analyser-test/src_test/com/axonivy/solutions/process/analyser/test/it/ProcessAnalyticsWebTest.java
+++ process-analyser-test/src_test/com/axonivy/solutions/process/analyser/test/it/ProcessAnalyticsWebTest.java
@@ -1,30 +1,33 @@
 package com.axonivy.solutions.process.analyser.test.it;
 
 import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
+import com.codeborne.selenide.SelenideElement;
 
 @IvyWebTest
 public class ProcessAnalyticsWebTest extends WebBaseSetup {
 
-  private final String SHOW_STATISTIC_BTN_CSS_SELECTOR = "#process-analytics-form\\:show-statistic-btn";
-  private final String MODULE_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:moduleDropdown";
-  private final String PROCESS_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:processDropdown";
-  private final String KPI_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:kpiDropdown";
-  private final String CASCADE_DROPDOWN_LIST_SUFFIX = "_panel";
-  private final String CASCADE_DROPDOWN_LABEL_CSS_SELECTOR_SUFFIX = " .ui-cascadeselect-label";
-  private final String DISABLE_PROPERTY = "disabled";
-  private final String PROCESS_NAME_EN = "Test process";
-  private final String PROCESS_NAME_DE = "Testprozess";
-  private final String FREQUENCY_OPTION_NAME = "Frequency";
-  private final String DROPDOWN_LABEL_SUFFIX = "_label";
-
-  private final String TEST_MODULE_NAME = "process-analyser-test";
-  private final String DROPDOWN_LIST_SUFFIX = "_items";
+  private static final String SHOW_STATISTIC_BTN_CSS_SELECTOR = "#process-analytics-form\\:show-statistic-btn";
+  private static final String MODULE_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:moduleDropdown";
+  private static final String PROCESS_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:process-dropdown";
+  private static final String KPI_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:kpiDropdown";
+  private static final String CASCADE_DROPDOWN_LIST_SUFFIX = "_panel";
+  private static final String CASCADE_DROPDOWN_LABEL_CSS_SELECTOR_SUFFIX = " .ui-cascadeselect-label";
+  private static final String DISABLE_PROPERTY = "disabled";
+  private static final String PROCESS_FILE = "TestProcess";
+  private static final String PROCESS_NAME_EN = "Test process";
+  private static final String PROCESS_NAME_DE = "Testprozess";
+  private static final String FREQUENCY_OPTION_NAME = "Frequency";
+  private static final String DROPDOWN_LABEL_SUFFIX = "_label";
+  private static final String TEST_MODULE_NAME = "process-analyser-test";
+  private static final String DROPDOWN_LIST_SUFFIX = "_items";
 
   @Test
   void showStatisticButtonShouldEnableWhenChosenFulfiled() {
@@ -38,8 +41,7 @@ public class ProcessAnalyticsWebTest extends WebBaseSetup {
     verifyAndClickItemLabelInDropdown(MODULE_DROPDOWN_CSS_SELECTOR, TEST_MODULE_NAME, DROPDOWN_LIST_SUFFIX,
         DROPDOWN_LABEL_SUFFIX);
     // Verify English process name is rendered
-    verifyAndClickItemLabelInDropdown(PROCESS_DROPDOWN_CSS_SELECTOR, PROCESS_NAME_EN, DROPDOWN_LIST_SUFFIX,
-        DROPDOWN_LABEL_SUFFIX);
+    verifyAndSelectAProcess(PROCESS_NAME_EN);
     verifyAndClickItemLabelInDropdown(KPI_DROPDOWN_CSS_SELECTOR, FREQUENCY_OPTION_NAME, CASCADE_DROPDOWN_LIST_SUFFIX,
         CASCADE_DROPDOWN_LABEL_CSS_SELECTOR_SUFFIX);
     // Check the status of show statistic button after data fulfilled
@@ -52,8 +54,29 @@ public class ProcessAnalyticsWebTest extends WebBaseSetup {
     verifyAndClickItemLabelInDropdown(MODULE_DROPDOWN_CSS_SELECTOR, TEST_MODULE_NAME, DROPDOWN_LIST_SUFFIX,
         DROPDOWN_LABEL_SUFFIX);
     // Verify German process name is rendered
-    verifyAndClickItemLabelInDropdown(PROCESS_DROPDOWN_CSS_SELECTOR, PROCESS_NAME_DE, DROPDOWN_LIST_SUFFIX,
-        DROPDOWN_LABEL_SUFFIX);
+    verifyAndSelectAProcess(PROCESS_NAME_DE);
     resetLocale();
+  }
+
+  private void verifyAndSelectAProcess(String startElementName) {
+    // Verify and click the process drop-down
+    var processDropdown = $(PROCESS_DROPDOWN_CSS_SELECTOR).shouldBe(visible, DEFAULT_DURATION);
+    processDropdown.click();
+
+    // Verify selection panel
+    var selectionPanel = $(PROCESS_DROPDOWN_CSS_SELECTOR + CASCADE_DROPDOWN_LIST_SUFFIX).shouldBe(visible, DEFAULT_DURATION);
+
+    // Find 1st option (index = 1 to avoid choosing default initial option of null)
+    SelenideElement targetElement = selectionPanel.$$(" li").stream().filter(item -> PROCESS_FILE.equals(item.text()))
+        .findAny().orElse(null);
+    targetElement.click();
+
+    // Find the sub selection and click on start element by name
+    var startElement = targetElement.find(By.cssSelector("ul.ui-cascadeselect-sublist"))
+        .$$(By.cssSelector("li.ui-cascadeselect-item")).stream()
+          .filter(element -> element.getText().contains(startElementName)).findAny()
+          .orElseThrow(() -> new AssertionError(String.join(StringUtils.SPACE, "Dropdown item with text", startElementName, "not found!")))
+          .shouldBe(visible, DEFAULT_DURATION);
+    startElement.click();
   }
 }
