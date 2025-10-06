20d66aeb7388dc07de81940aeae9b88e311cfb0f
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-10-06 13:37:20 +0700

    Update ProcessAnalyticViewComponentId.java

--- process-analyser/src/com/axonivy/solutions/process/analyser/constants/ProcessAnalyticViewComponentId.java
+++ process-analyser/src/com/axonivy/solutions/process/analyser/constants/ProcessAnalyticViewComponentId.java
@@ -3,10 +3,11 @@ package com.axonivy.solutions.process.analyser.constants;
 import java.util.List;
 
 public class ProcessAnalyticViewComponentId {
-  private static final String FORM = "process-analytics-form";
   public static final String ID_SECTION_SEPARATOR = ":";
-  public static final String PROCESS_SELECTION_GROUP = FORM + ID_SECTION_SEPARATOR + "process-selection-group";
-  public static final String ARROW_STATISTIC = FORM + ID_SECTION_SEPARATOR + "arrow-statistics";
+  private static final String FORM = "process-analytics-form";
+  private static final String STANDARD_FILTER_PANEL_GROUP = FORM + ID_SECTION_SEPARATOR + "standard-filter-panel-group";
+  public static final String PROCESS_SELECTION_GROUP = STANDARD_FILTER_PANEL_GROUP + ID_SECTION_SEPARATOR
+      + "process-selection-group";
   public static final String HIDDEN_IMAGE = FORM + ID_SECTION_SEPARATOR + "hidden-image";
   public static final String PROCESS_ANALYTIC_VIEWER_PANEL = FORM + ID_SECTION_SEPARATOR
       + "process-analytic-viewer-panel" + ID_SECTION_SEPARATOR + "viewer-group";
@@ -18,7 +19,7 @@ public class ProcessAnalyticViewComponentId {
   public static final String ACTION_BTN_GROUP = FORM + ID_SECTION_SEPARATOR + "action-btn-group";
   public static final String DATA_STATISTICS = FORM + ID_SECTION_SEPARATOR + "data-statistics";
   public static List<String> getDiagramAndStatisticComponentIds() {
-    return List.of(ARROW_STATISTIC, HIDDEN_IMAGE, PROCESS_ANALYTIC_VIEWER_PANEL, SHOW_STATISTIC_BTN, ACTION_BTN_GROUP,
+    return List.of(HIDDEN_IMAGE, PROCESS_ANALYTIC_VIEWER_PANEL, SHOW_STATISTIC_BTN, ACTION_BTN_GROUP,
         DATA_STATISTICS);
   }
 }
