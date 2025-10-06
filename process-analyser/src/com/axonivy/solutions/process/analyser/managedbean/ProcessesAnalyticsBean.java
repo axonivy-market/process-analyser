26ffa7f2089b18ce968ca9bcc9a471c9954fe3e1
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-10-06 11:11:02 +0700

    format code

--- process-analyser/src/com/axonivy/solutions/process/analyser/managedbean/ProcessesAnalyticsBean.java
+++ process-analyser/src/com/axonivy/solutions/process/analyser/managedbean/ProcessesAnalyticsBean.java
@@ -23,7 +23,6 @@ import javax.faces.model.SelectItem;
 import javax.faces.model.SelectItemGroup;
 
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.primefaces.PF;
@@ -289,7 +288,6 @@ public class ProcessesAnalyticsBean {
       updateUserProperty(UserProperty.WIDGET_SELECTED_KPI, selectedKpiType.name());
     }
     colorPickerBean.initBean(selectedKpiType, selectedColorMode);
-    colorPickerBean.getBackgroundAndTextColors();
     refreshAnalyserReportToView();
   }
 
