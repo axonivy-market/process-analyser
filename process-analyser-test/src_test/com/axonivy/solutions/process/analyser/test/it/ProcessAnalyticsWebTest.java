d1fbb22ada78a151141bdd87f9275ce4baab05a6
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-11-13 16:42:36 +0700

    MARP-3143 Update UT

--- process-analyser-test/src_test/com/axonivy/solutions/process/analyser/test/it/ProcessAnalyticsWebTest.java
+++ process-analyser-test/src_test/com/axonivy/solutions/process/analyser/test/it/ProcessAnalyticsWebTest.java
@@ -19,7 +19,6 @@ public class ProcessAnalyticsWebTest extends WebBaseSetup {
   private static final String MODULE_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:standard-filter-panel-group\\:moduleDropdown";
   private static final String PROCESS_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:standard-filter-panel-group\\:process-dropdown";
   private static final String KPI_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:standard-filter-panel-group\\:kpiDropdown";
-  private static final String PMV_DROPDOWN_CSS_SELECTOR = "#process-analytics-form\\:standard-filter-panel-group\\:pmvDropdown";
   private static final String CASCADE_DROPDOWN_LIST_SUFFIX = "_panel";
   private static final String CASCADE_DROPDOWN_LABEL_CSS_SELECTOR_SUFFIX = " .ui-cascadeselect-label";
   private static final String DISABLE_PROPERTY = "disabled";
@@ -31,34 +30,34 @@ public class ProcessAnalyticsWebTest extends WebBaseSetup {
   private static final String TEST_MODULE_NAME = "process-analyser-test";
   private static final String DROPDOWN_LIST_SUFFIX = "_items";
 
-   @Test
-   void showStatisticButtonShouldEnableWhenChosenFulfiled() {
-   login();
-   resetLocale();
-   startAnalyzingProcess();
-   // Check the current status of show statistic button
-   $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute(DISABLE_PROPERTY, "true"));
-  
-   // Choose test project PM
-   verifyAndClickItemLabelInDropdown(MODULE_DROPDOWN_CSS_SELECTOR, TEST_MODULE_NAME, DROPDOWN_LIST_SUFFIX,
-   DROPDOWN_LABEL_SUFFIX);
-   // Verify English process name is rendered
-   verifyAndSelectAProcess(PROCESS_NAME_EN);
-   verifyAndClickItemLabelInDropdown(KPI_DROPDOWN_CSS_SELECTOR, FREQUENCY_OPTION_NAME, CASCADE_DROPDOWN_LIST_SUFFIX,
-   CASCADE_DROPDOWN_LABEL_CSS_SELECTOR_SUFFIX);
-   // Check the status of show statistic button after data fulfilled
-   $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute(DISABLE_PROPERTY, StringUtils.EMPTY));
-  
-   // Change locale
-   changeLocaleToGerman();
-  
-   startAnalyzingProcess();
-   verifyAndClickItemLabelInDropdown(PMV_DROPDOWN_CSS_SELECTOR, TEST_MODULE_NAME, DROPDOWN_LIST_SUFFIX,
-   DROPDOWN_LABEL_SUFFIX);
-   // Verify German process name is rendered
-   verifyAndSelectAProcess(PROCESS_NAME_DE);
-   resetLocale();
-   }
+  @Test
+  void showStatisticButtonShouldEnableWhenChosenFulfiled() {
+    login();
+    resetLocale();
+    startAnalyzingProcess();
+    // Check the current status of show statistic button
+    $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute(DISABLE_PROPERTY, "true"));
+
+    // Choose test project PM
+    verifyAndClickItemLabelInDropdown(MODULE_DROPDOWN_CSS_SELECTOR, TEST_MODULE_NAME, DROPDOWN_LIST_SUFFIX,
+        DROPDOWN_LABEL_SUFFIX);
+    // Verify English process name is rendered
+    verifyAndSelectAProcess(PROCESS_NAME_EN);
+    verifyAndClickItemLabelInDropdown(KPI_DROPDOWN_CSS_SELECTOR, FREQUENCY_OPTION_NAME, CASCADE_DROPDOWN_LIST_SUFFIX,
+        CASCADE_DROPDOWN_LABEL_CSS_SELECTOR_SUFFIX);
+    // Check the status of show statistic button after data fulfilled
+    $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute(DISABLE_PROPERTY, StringUtils.EMPTY));
+
+    // Change locale
+    changeLocaleToGerman();
+
+    startAnalyzingProcess();
+    verifyAndClickItemLabelInDropdown(MODULE_DROPDOWN_CSS_SELECTOR, TEST_MODULE_NAME, DROPDOWN_LIST_SUFFIX,
+        DROPDOWN_LABEL_SUFFIX);
+    // Verify German process name is rendered
+    verifyAndSelectAProcess(PROCESS_NAME_DE);
+    resetLocale();
+  }
 
   @Test
   void showPmvWhenSelectProcess() {
