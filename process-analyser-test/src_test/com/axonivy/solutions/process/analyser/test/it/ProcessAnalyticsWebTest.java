524e6b9296e0fc3735a3dda26ed735e879affbb2
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-11-13 16:32:28 +0700

    MARP-3143 Update UT

--- process-analyser-test/src_test/com/axonivy/solutions/process/analyser/test/it/ProcessAnalyticsWebTest.java
+++ process-analyser-test/src_test/com/axonivy/solutions/process/analyser/test/it/ProcessAnalyticsWebTest.java
@@ -6,11 +6,13 @@ import static com.codeborne.selenide.Selenide.$;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
-@IvyWebTest(headless = false)
+@IvyWebTest
 public class ProcessAnalyticsWebTest extends WebBaseSetup {
 
   private static final String SHOW_STATISTIC_BTN_CSS_SELECTOR = "#process-analytics-form\\:standard-filter-panel-group\\:show-statistic-btn";
@@ -27,55 +29,50 @@ public class ProcessAnalyticsWebTest extends WebBaseSetup {
   private static final String FREQUENCY_OPTION_NAME = "Frequency";
   private static final String DROPDOWN_LABEL_SUFFIX = "_label";
   private static final String TEST_MODULE_NAME = "process-analyser-test";
-  private static final String INTERNAL_SUPPORT_MODULE = "process-analyser-internal-support";
   private static final String DROPDOWN_LIST_SUFFIX = "_items";
 
-//  @Test
-//  void showStatisticButtonShouldEnableWhenChosenFulfiled() {
-//    login();
-//    resetLocale();
-//    startAnalyzingProcess();
-//    // Check the current status of show statistic button
-//    $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute(DISABLE_PROPERTY, "true"));
-//
-//    // Choose test project PM
-//    verifyAndClickItemLabelInDropdown(MODULE_DROPDOWN_CSS_SELECTOR, TEST_MODULE_NAME, DROPDOWN_LIST_SUFFIX,
-//        DROPDOWN_LABEL_SUFFIX);
-//    // Verify English process name is rendered
-//    verifyAndSelectAProcess(PROCESS_NAME_EN);
-//    verifyAndClickItemLabelInDropdown(KPI_DROPDOWN_CSS_SELECTOR, FREQUENCY_OPTION_NAME, CASCADE_DROPDOWN_LIST_SUFFIX,
-//        CASCADE_DROPDOWN_LABEL_CSS_SELECTOR_SUFFIX);
-//    // Check the status of show statistic button after data fulfilled
-//    $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute(DISABLE_PROPERTY, StringUtils.EMPTY));
-//
-//    // Change locale
-//    changeLocaleToGerman();
-//
-//    startAnalyzingProcess();
-//    verifyAndClickItemLabelInDropdown(PMV_DROPDOWN_CSS_SELECTOR, TEST_MODULE_NAME, DROPDOWN_LIST_SUFFIX,
-//        DROPDOWN_LABEL_SUFFIX);
-//    // Verify German process name is rendered
-//    verifyAndSelectAProcess(PROCESS_NAME_DE);
-//    resetLocale();
-//  }
+   @Test
+   void showStatisticButtonShouldEnableWhenChosenFulfiled() {
+   login();
+   resetLocale();
+   startAnalyzingProcess();
+   // Check the current status of show statistic button
+   $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute(DISABLE_PROPERTY, "true"));
   
+   // Choose test project PM
+   verifyAndClickItemLabelInDropdown(MODULE_DROPDOWN_CSS_SELECTOR, TEST_MODULE_NAME, DROPDOWN_LIST_SUFFIX,
+   DROPDOWN_LABEL_SUFFIX);
+   // Verify English process name is rendered
+   verifyAndSelectAProcess(PROCESS_NAME_EN);
+   verifyAndClickItemLabelInDropdown(KPI_DROPDOWN_CSS_SELECTOR, FREQUENCY_OPTION_NAME, CASCADE_DROPDOWN_LIST_SUFFIX,
+   CASCADE_DROPDOWN_LABEL_CSS_SELECTOR_SUFFIX);
+   // Check the status of show statistic button after data fulfilled
+   $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute(DISABLE_PROPERTY, StringUtils.EMPTY));
+  
+   // Change locale
+   changeLocaleToGerman();
+  
+   startAnalyzingProcess();
+   verifyAndClickItemLabelInDropdown(PMV_DROPDOWN_CSS_SELECTOR, TEST_MODULE_NAME, DROPDOWN_LIST_SUFFIX,
+   DROPDOWN_LABEL_SUFFIX);
+   // Verify German process name is rendered
+   verifyAndSelectAProcess(PROCESS_NAME_DE);
+   resetLocale();
+   }
+
   @Test
   void showPmvWhenSelectProcess() {
     login();
     startAnalyzingProcess();
-    // Check the current status of show statistic button
     $(SHOW_STATISTIC_BTN_CSS_SELECTOR).shouldBe(attribute(DISABLE_PROPERTY, "true"));
 
-    // Choose test project PM
-    var processDropdown = $(INTERNAL_SUPPORT_MODULE).shouldBe(visible, DEFAULT_DURATION);
-    processDropdown.click();
-    var pmvSelectionPanel = $(PMV_DROPDOWN_CSS_SELECTOR).shouldBe(visible, DEFAULT_DURATION);
-    var processFileAndStartName = "1";
-    SelenideElement targetElement =
-        pmvSelectionPanel.$$(" li").stream().filter(item -> processFileAndStartName.equals(item.text())).findAny()
-            .orElseThrow(() -> new AssertionError(getDropdownItemNotFoundMessage(processFileAndStartName)))
-            .shouldBe(visible, DEFAULT_DURATION);
-    targetElement.click();
+    $(By.id("process-analytics-form:standard-filter-panel-group:moduleDropdown")).click();
+    $(By.id("process-analytics-form:standard-filter-panel-group:moduleDropdown_1")).click();
+    Selenide.sleep(5000);
+    $(By.id("process-analytics-form:standard-filter-panel-group:pmvDropdown")).click();
+    $(By.id("process-analytics-form:standard-filter-panel-group:pmvDropdown_1")).click();
+    $(By.id("process-analytics-form:standard-filter-panel-group:pmvDropdown")).click();
+    $(By.id("process-analytics-form:standard-filter-panel-group:pmvDropdown_2")).click();
   }
 
   private void verifyAndSelectAProcess(String startElementName) {
@@ -84,14 +81,15 @@ public class ProcessAnalyticsWebTest extends WebBaseSetup {
     processDropdown.click();
 
     // Verify selection panel
-    var selectionPanel = $(PROCESS_DROPDOWN_CSS_SELECTOR + CASCADE_DROPDOWN_LIST_SUFFIX).shouldBe(visible, DEFAULT_DURATION);
+    var selectionPanel =
+        $(PROCESS_DROPDOWN_CSS_SELECTOR + CASCADE_DROPDOWN_LIST_SUFFIX).shouldBe(visible, DEFAULT_DURATION);
 
     // Find 1st option (index = 1 to avoid choosing default initial option of null)
     var processFileAndStartName = PROCESS_FILE.concat("/").concat("Start: ").concat(startElementName);
-    SelenideElement targetElement = selectionPanel.$$(" li").stream()
-        .filter(item -> processFileAndStartName.equals(item.text())).findAny()
-        .orElseThrow(() -> new AssertionError(getDropdownItemNotFoundMessage(processFileAndStartName)))
-        .shouldBe(visible, DEFAULT_DURATION);
+    SelenideElement targetElement =
+        selectionPanel.$$(" li").stream().filter(item -> processFileAndStartName.equals(item.text())).findAny()
+            .orElseThrow(() -> new AssertionError(getDropdownItemNotFoundMessage(processFileAndStartName)))
+            .shouldBe(visible, DEFAULT_DURATION);
     targetElement.click();
   }
 }
