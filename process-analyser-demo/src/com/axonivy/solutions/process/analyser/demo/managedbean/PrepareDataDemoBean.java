abcb6e13747f8dec4a30ac7b791328c05f445cfd
ttan-axonivy <an.truongthien@axonactive.com>
2025-11-24 18:20:24 +0700

    MARP-3142 Enhance demo - Init Demo data on startup (#159)

---
+++ process-analyser-demo/src/com/axonivy/solutions/process/analyser/demo/managedbean/PrepareDataDemoBean.java
@@ -0,0 +1,32 @@
+package com.axonivy.solutions.process.analyser.demo.managedbean;
+
+import java.util.List;
+
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.process.eventstart.AbstractProcessStartEventBean;
+import ch.ivyteam.ivy.process.eventstart.IProcessStartEventBeanRuntime;
+import ch.ivyteam.ivy.process.extension.ProgramConfig;
+
+public class PrepareDataDemoBean extends AbstractProcessStartEventBean {
+  private static final int SIGNAL_SEND_REPEAT = 5;
+  private static final List<String> SIGNAL_LIST = List.of("hotelBooking", "bookFlight", "flightPayment", "planFlight", "recommendDestination",
+      "mortgate:purchase", "initiateDataFromQueue");
+
+  public PrepareDataDemoBean() {
+    super("PrepareDataDemoBean", "Description of PrepareDataDemoBean");
+  }
+
+  @Override
+  public void initialize(IProcessStartEventBeanRuntime eventRuntime, ProgramConfig programConfig) {
+    super.initialize(eventRuntime, programConfig);
+    sendSignals();
+  }
+
+  public static void sendSignals() {
+    for (int i = 0; i < SIGNAL_SEND_REPEAT; i++) {
+      for (String signal : SIGNAL_LIST) {
+        Ivy.wf().signals().create().send(signal);
+      }
+    }
+  }
+}
