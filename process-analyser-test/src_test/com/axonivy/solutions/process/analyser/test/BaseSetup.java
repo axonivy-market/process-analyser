f920d0f53d1cd06eb37a5a0a92c548527f95051d
nqhoan-axonivy <hoan.nguyen01@axonactive.com>
2026-08-05 17:27:59 +0700

    fix: change to project api for pmv

--- process-analyser-test/src_test/com/axonivy/solutions/process/analyser/test/BaseSetup.java
+++ process-analyser-test/src_test/com/axonivy/solutions/process/analyser/test/BaseSetup.java
@@ -7,16 +7,15 @@ import java.util.regex.Pattern;
 import org.apache.commons.lang3.Strings;
 
 import com.axonivy.solutions.process.analyser.bo.IvyProcess;
-import com.axonivy.solutions.process.analyser.internal.ProcessUtils;
-import ch.ivyteam.ivy.application.project.Project;
-
 import static com.axonivy.solutions.process.analyser.constants.AnalyserConstants.RGB_REGEX_PATTERN;
+import com.axonivy.solutions.process.analyser.internal.ProcessUtils;
 
+import ch.ivyteam.ivy.application.app.Application;
+import ch.ivyteam.ivy.application.project.Project;
 import ch.ivyteam.ivy.process.model.NodeElement;
 import ch.ivyteam.ivy.process.model.connector.SequenceFlow;
 import ch.ivyteam.ivy.process.model.element.ProcessElement;
 
-@SuppressWarnings("restriction")
 public class BaseSetup {
   protected static final String TEST_REQUEST_PATH = "193485C5ABDFEA93/193485C5ABDFEA93-f0/test.ivp";
   protected static final String TEST_PROCESS_ELEMENT_START_PID = "193485C5ABDFEA93-f0";
@@ -51,7 +50,7 @@ public class BaseSetup {
   protected static Project testPMV;
   
   protected static void prepareData() {
-    testPMV = Application.current().getProcessModelVersions().toList().get(0);
+    testPMV = Application.current().projects().all().toList().get(0);
     testProcess = ProcessUtils.getAllProcesses().stream()
         .filter(start -> Strings.CS.contains(start.getName(), TEST_PROCESS_NAME)).findAny().orElse(null);
     testProcessElements = ProcessUtils.getProcessElementsFrom(testProcess.getId(), testProcess.getPmv());
