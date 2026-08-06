e96e0553d44d7a4e79b3830cd12a0e50300f1c4f
Hoan Nguyen <83745591+nqhoan-axonivy@users.noreply.github.com>
2026-08-06 16:05:32 +0700

    MARP-4660 feat: migrate to Jakarta (#210)

--- process-analyser-core/src/com/axonivy/solutions/process/analyser/core/internal/ProcessViewerBuilder.java
+++ process-analyser-core/src/com/axonivy/solutions/process/analyser/core/internal/ProcessViewerBuilder.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import javax.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriBuilder;
 
 import org.apache.commons.lang3.StringUtils;
 
